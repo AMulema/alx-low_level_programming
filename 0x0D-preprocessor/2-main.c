d9084412f8490aa491ba90e4ab064a80062df5f1
AMulema <austinmulema27@gmail.com>
2023-03-20 04:18:57 -0700

    code for 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define _FILE_
+
+/**
+ * main - prints the name of
+ * the file it was compiled
+ * from followed by a new
+ * line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
